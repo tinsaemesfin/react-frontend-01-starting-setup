3e9ec22d4b1d49ebe254b7ac8b31fb7f9ce766e1
tinsaemesfin <qmesfin@gmail.com>
2022-05-04 19:52:17 +0300

    First Comment

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import NewPlace from "./places/pages/NewPlace";
+import Users from "./users/pages/Users";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Users />} />
+        <Route path="/places/new" exact element={<NewPlace />} />
+        <Route path="*" exact element={<Users />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
