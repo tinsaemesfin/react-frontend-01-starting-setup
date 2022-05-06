1c22f67be9b63c2da6d77148b76c67b6e7c7fd79
tinsaemesfin <qmesfin@gmail.com>
2022-05-06 15:53:23 +0300

    end of ep-09

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import NavLinks from './NavLinks';
+import React from "react";
+import { Link } from "react-router-dom";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import MainHeader from "./MainHeader";
+import "./MainNavigation.css";
 
-import MainHeader from './MainHeader';
-import './MainNavigation.css';
-
-const MainNavigation = props => {
+const MainNavigation = (props) => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
-      </h1>
-      <nav>
-       <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
